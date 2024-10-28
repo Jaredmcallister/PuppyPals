5484b477df8b7c9b240bb5bc30084075054dabec
Jared Mc Allister <jaredmcallister@Jareds-Laptop.attlocal.net>
2024-10-28 16:18:45 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
- 
 
-  return (
+function App() {
+ console.log(puppyList);
+ const [puppies, setPuppies] = useState(puppyList)
+ console.log(puppies); 
+ return (
+  
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        {
+          puppies.map((puppy) =>{
+            return <p>{puppy.name}</p>
+          })
+        }
        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      </>
   )
 }
 
